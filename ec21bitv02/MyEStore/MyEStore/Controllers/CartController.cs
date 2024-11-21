17f744599426ef44b81d2c010c1d0a7c0e3f0c57
Finn9523 <luongngocthien.9a2xt@gmail.com>
2024-11-21 22:33:41 +0700

    gộp code phần giỏ hàng

--- ec21bitv02/MyEStore/MyEStore/Controllers/CartController.cs
+++ ec21bitv02/MyEStore/MyEStore/Controllers/CartController.cs
@@ -60,20 +60,59 @@ namespace MyEStore.Controllers
 				cart.Add(cartItem);
 			}
 			HttpContext.Session.Set(CART_KEY, cart);
-			return RedirectToAction("Index");
+			return RedirectToAction("Index", "Products");
 		}
 
 		public IActionResult RemoveCartItem(int id)
 		{
 			var cart = CartItems;
 			var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
+
 			if (cartItem != null)
 			{
 				cart.Remove(cartItem);
 				HttpContext.Session.Set(CART_KEY, cart);
+				TempData["ThongBao"] = "Sản phẩm đã được xóa khỏi giỏ hàng.";
 			}
+			else
+			{
+				TempData["ThongBao"] = "Không tìm thấy sản phẩm trong giỏ hàng.";
+			}
+
+			return RedirectToAction("Index");
+		}
+		[HttpPost]
+		public IActionResult UpdateCart(int id, int qty)
+		{
+			if (qty <= 0)
+			{
+				TempData["ThongBao"] = "Số lượng phải lớn hơn 0.";
+				return RedirectToAction("Index");
+			}
+
+			var cart = CartItems;
+			var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
+
+			if (cartItem != null)
+			{
+				cartItem.SoLuong = qty;
+				HttpContext.Session.Set(CART_KEY, cart);
+				TempData["ThongBao"] = "Số lượng sản phẩm đã được cập nhật.";
+			}
+			else
+			{
+				TempData["ThongBao"] = "Không tìm thấy sản phẩm trong giỏ hàng.";
+			}
+
 			return RedirectToAction("Index");
 		}
 
+		// Xóa toàn bộ giỏ hàng
+		public IActionResult ClearCart()
+		{
+			HttpContext.Session.Remove(CART_KEY);
+			TempData["ThongBao"] = "Giỏ hàng đã được làm trống.";
+			return RedirectToAction("Index");
+		}
 	}
 }
