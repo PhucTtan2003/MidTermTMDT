3dc950196abfcfaf5e0ba31f823e33de29752870
PhucTtan2003 <141216726+nguyenvanthinh992003@users.noreply.github.com>
2024-11-21 21:10:27 +0700

    uodate myestore

--- MyEStore/MyEStore/Controllers/ProductsController.cs
+++ MyEStore/MyEStore/Controllers/ProductsController.cs
@@ -4,74 +4,41 @@ using MyEStore.Models;
 
 namespace MyEStore.Controllers
 {
-	public class ProductsController : Controller
-	{
-		private readonly MyeStoreContext _ctx;
+    public class ProductsController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
 
-		public ProductsController(MyeStoreContext ctx)
-		{
-			_ctx = ctx;
-		}
-
-		public IActionResult Detail(int id)
-		{
-			var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
-			if (hangHoa == null)
-			{
-				return NotFound();
-			}
-			return View(hangHoa);
-		}
-
-		public IActionResult Index(int? cateid)
-		{
-			var data = _ctx.HangHoas.AsQueryable();
-			if (cateid.HasValue)
-			{
-				data = data.Where(hh => hh.MaLoai == cateid.Value);
-			}
-
-			var result = data.Select(hh => new HangHoaVM
-			{
-				MaHh = hh.MaHh,
-				TenHh = hh.TenHh,
-				DonGia = hh.DonGia ?? 0,
-				Hinh = hh.Hinh
-			});
-			return View(result);
-		}
-
-        [HttpPost]
-        public IActionResult Edit(HangHoa model)
+        public ProductsController(MyeStoreContext ctx)
         {
-            if (!ModelState.IsValid)
-            {
-                // Log or inspect model state errors
-                var errors = ModelState.Values.SelectMany(v => v.Errors);
-                foreach (var error in errors)
-                {
-                    Console.WriteLine(error.ErrorMessage);
-                }
-                return View(model);
-            }
+            _ctx = ctx;
+        }
 
-            var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == model.MaHh);
+        public IActionResult Detail(int id)
+        {
+            var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
             if (hangHoa == null)
             {
                 return NotFound();
             }
-
-            // Update the properties
-            hangHoa.TenHh = model.TenHh;
-            hangHoa.DonGia = model.DonGia;
-            hangHoa.Hinh = model.Hinh;
-
-            _ctx.SaveChanges();
-
-            return RedirectToAction("Index");
+            return View(hangHoa);
         }
 
+        public IActionResult Index(int? cateid)
+        {
+            var data = _ctx.HangHoas.AsQueryable();
+            if (cateid.HasValue)
+            {
+                data = data.Where(hh => hh.MaLoai == cateid.Value);
+            }
 
-
+            var result = data.Select(hh => new HangHoaVM
+            {
+                MaHh = hh.MaHh,
+                TenHh = hh.TenHh,
+                DonGia = hh.DonGia ?? 0,
+                Hinh = hh.Hinh
+            });
+            return View(result);
+        }
     }
 }
